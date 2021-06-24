11f3e9dc69bf61e3aa80a37f1b10314d73633b79
ShiroKatake <tgkhang262@gmail.com>
2021-06-24 12:43:50 +1000

    UPDATE: Plugged into main

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 //import HelloWorld from './2. HelloWorld/HelloWorld'
 //import ComponentPractice from './3. ComponentPractice/App'
 import TodoList from './3. TodoList/App'
+import ContactList from './6. ContactList/App'
 
-ReactDOM.render(<TodoList />, document.getElementById("root"));
+ReactDOM.render(<ContactList />, document.getElementById("root"));
