452a74f1e37c9b1ba201511fec0af41d756be9ee
ShiroKatake <tgkhang262@gmail.com>
2021-06-24 12:21:04 +1000

    UPDATE: Added numbering so we know what exercise belongs to what lesson

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import HelloWorld from './HelloWorld/HelloWorld'
-//import ComponentPractice from './ComponentPractice/App'
-import TodoList from './TodoList/App'
+//import HelloWorld from './2. HelloWorld/HelloWorld'
+//import ComponentPractice from './3. ComponentPractice/App'
+import TodoList from './3. TodoList/App'
 
 ReactDOM.render(<TodoList />, document.getElementById("root"));
