782b0b14f25aacd20e477e6920d18ec3b4e28f82
ShiroKatake <tgkhang262@gmail.com>
2021-06-24 12:43:43 +1000

    UPDATE: Implemented contact details for element

--- src/6. ContactList/components/ContactElement.js
+++ src/6. ContactList/components/ContactElement.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function ContactElement() {
+function ContactElement(props) {
     return (
         <div>
-            <img/>
-            <h3></h3>
-            <p></p>
-            <p></p>
+            <img src={props.contact.imgUrl}/>
+            <h3>{props.contact.name}</h3>
+            <p>Phone: {props.contact.phone}</p>
+            <p>Email: {props.contact.email}</p>
         </div>
     )
 }
