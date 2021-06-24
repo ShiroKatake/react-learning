b7fc231a90ac7886f3370cbb5055159de901eab6
ShiroKatake <tgkhang262@gmail.com>
2021-06-24 12:27:04 +1000

    ADD: Basic main for contact list

---
+++ src/6. ContactList/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactElement from './components/ContactElement'
+
+function App() {
+    return (
+        <div>
+            <ContactElement />
+        </div>
+    )
+}
+
+export default App
