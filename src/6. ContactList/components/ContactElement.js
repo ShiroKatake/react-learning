60571a45cea54241efc91b453fdf670766f24ba0
ShiroKatake <tgkhang262@gmail.com>
2021-06-24 12:26:50 +1000

    ADD: Basic structure for a contact element

---
+++ src/6. ContactList/components/ContactElement.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ContactElement() {
+    return (
+        <div>
+            <img/>
+            <h3></h3>
+            <p></p>
+            <p></p>
+        </div>
+    )
+}
+
+export default ContactElement
