8f0d6e5787e7dc2a5a96c3df05f3588b3abfca47
ShiroKatake <tgkhang262@gmail.com>
2021-07-11 11:54:00 +1000

    UPDATE: Renamed files for easy distinction

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 //import HelloWorld from './2. HelloWorld/HelloWorld'
-//import ComponentPractice from './3. ComponentPractice/App'
-//import TodoList from './3. TodoList/App'
-//import ContactList from './6. ContactList/App'
-import JokeList from "./6. JokeDisplay/App"
+//import ComponentPractice from './3. ComponentPractice/ComponentPractice_App'
+//import TodoList from './3. TodoList/TodoList_App'
+//import ContactList from './6. ContactList/ContactList_App'
+import JokeList from "./6. JokeDisplay/JokeList_App"
 
 ReactDOM.render(<JokeList />, document.getElementById("root"));
