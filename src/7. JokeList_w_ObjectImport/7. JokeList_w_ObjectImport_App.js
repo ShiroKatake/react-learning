db9aead43b845b4ade520e63be7a53c601c451f2
ShiroKatake <tgkhang262@gmail.com>
2021-07-12 17:44:58 +1000

    ADD: Duplicated task 6 to make template for task 7

---
+++ src/7. JokeList_w_ObjectImport/7. JokeList_w_ObjectImport_App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Joke from "../6. JokeDisplay/components/Joke";
+
+function App() {
+	return (
+		<ol>
+			<Joke
+				elements={{
+					question: "What's the best thing about Switzerland?",
+					punchline: "I don't know, but the flag is a big plus.",
+				}}
+			/>
+			<Joke
+				elements={{
+					question: "Why do we tell actors to 'break a leg'?",
+					punchline: "Because every play has a cast.",
+				}}
+			/>
+		</ol>
+	);
+}
+
+export default App;
