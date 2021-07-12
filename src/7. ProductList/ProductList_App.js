ee6309dbb13aefd223d8f39243c0cf846b876b16
ShiroKatake <tgkhang262@gmail.com>
2021-07-12 17:54:50 +1000

    UPDATE: Renamed file

--- src/7. ProductList/ProductList_App.js
+++ src/7. ProductList/ProductList_App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import Joke from "../6. JokeDisplay/components/Joke";
+import Product from "../6. JokeDisplay/components/Joke";
 
 function App() {
 	return (
 		<ol>
-			<Joke
+			<Product
 				elements={{
 					question: "What's the best thing about Switzerland?",
 					punchline: "I don't know, but the flag is a big plus.",
 				}}
 			/>
-			<Joke
+			<Product
 				elements={{
 					question: "Why do we tell actors to 'break a leg'?",
 					punchline: "Because every play has a cast.",
