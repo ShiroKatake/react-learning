2e08208872c3553227610cc137e44b6cedcf208e
ShiroKatake <tgkhang262@gmail.com>
2021-07-17 18:26:08 +1000

    UPDATE: Converted TodoItem into class-based

--- src/3. TodoList/components/TodoItem.js
+++ src/3. TodoList/components/TodoItem.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-function TodoItem() {
-	return (
-		<div className="todo-item">
-			<input type="checkbox" id="clean" name="isHouseCleaned" />
-			<label for="clean"> Placeholder text.</label>
-			<br />
-		</div>
-	);
+class TodoItem extends React.Component {
+	render() {
+		return (
+			<div className="todo-item">
+				<input type="checkbox" id="clean" name="isHouseCleaned" />
+				<label for="clean"> Placeholder text.</label>
+				<br />
+			</div>
+		);
+	}
 }
 
 export default TodoItem;
