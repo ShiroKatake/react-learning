128100b1ffb86e2e1068431da71e8963a3a6779b
ShiroKatake <tgkhang262@gmail.com>
2021-07-17 18:21:21 +1000

    UPDATE: Converted HelloWorld into a class

--- src/2. HelloWorld/HelloWorld.js
+++ src/2. HelloWorld/HelloWorld.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-function HelloWorld() {
-	return (
-		<div>
-			<h1>Hello World.</h1>
-			<p>This is a paragraph.</p>
-		</div>
-	);
+class HelloWorld extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello World.</h1>
+				<p>This is a paragraph.</p>
+			</div>
+		);
+	}
 }
 
 export default HelloWorld;
