801d578067ef47e8bd5e0d166ea094329856ad24
ShiroKatake <tgkhang262@gmail.com>
2021-07-17 18:22:25 +1000

    UPDATE: Confirmed conversion of HelloWorld successful

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import HelloWorld from './2. HelloWorld/HelloWorld'
+import HelloWorld from "./2. HelloWorld/HelloWorld";
 //import ComponentPractice from './3. ComponentPractice/ComponentPractice_App'
 //import TodoList from './3. TodoList/TodoList_App'
 //import ContactList from './6. ContactList/ContactList_App'
 //import JokeList from "./6. JokeDisplay/JokeList_App";
-import ProductList from "./7. ProductList/ProductList_App";
+//import ProductList from "./7. ProductList/ProductList_App";
 
-ReactDOM.render(<ProductList />, document.getElementById("root"));
+ReactDOM.render(<HelloWorld />, document.getElementById("root"));
