7c5b77c4e08eceece35adc17a12b0937e6db1b70
ShiroKatake <tgkhang262@gmail.com>
2021-07-17 18:33:16 +1000

    UPDATE: Converted JokeList into class-based

--- src/6. JokeDisplay/JokeList_App.js
+++ src/6. JokeDisplay/JokeList_App.js
@@ -1,23 +1,25 @@
 import React from "react";
 import Joke from "./components/Joke";
 
-function App() {
-	return (
-		<ol>
-			<Joke
-				elements={{
-					question: "What's the best thing about Switzerland?",
-					punchline: "I don't know, but the flag is a big plus.",
-				}}
-			/>
-			<Joke
-				elements={{
-					question: "Why do we tell actors to 'break a leg'?",
-					punchline: "Because every play has a cast.",
-				}}
-			/>
-		</ol>
-	);
+class App extends React.Component {
+	render() {
+		return (
+			<ol>
+				<Joke
+					elements={{
+						question: "What's the best thing about Switzerland?",
+						punchline: "I don't know, but the flag is a big plus.",
+					}}
+				/>
+				<Joke
+					elements={{
+						question: "Why do we tell actors to 'break a leg'?",
+						punchline: "Because every play has a cast.",
+					}}
+				/>
+			</ol>
+		);
+	}
 }
 
 export default App;
--- src/6. JokeDisplay/components/Joke.js
+++ src/6. JokeDisplay/components/Joke.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-function Joke(props) {
-	return (
-		<li>
-			<h2>{props.elements.question}</h2>
-			<p>{props.elements.punchline}</p>
-			<hr />
-		</li>
-	);
+class Joke extends React.Component {
+	render() {
+		return (
+			<li>
+				<h2>{props.elements.question}</h2>
+				<p>{props.elements.punchline}</p>
+				<hr />
+			</li>
+		);
+	}
 }
 
 export default Joke;
