5b9bac9c0408a0dc8486cd8959a6ad571c8b2870
ShiroKatake <tgkhang262@gmail.com>
2021-07-17 18:27:35 +1000

    UPDATE: Converted ToDoList App to class-based

--- src/3. TodoList/TodoList_App.js
+++ src/3. TodoList/TodoList_App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import TodoComponent from "./components/TodoItem";
 
-function App() {
-	return (
-		<div className="todo-list">
-			<TodoComponent />
-			<TodoComponent />
-			<TodoComponent />
-		</div>
-	);
+class App extends React.Component {
+	render() {
+		return (
+			<div className="todo-list">
+				<TodoComponent />
+				<TodoComponent />
+				<TodoComponent />
+			</div>
+		);
+	}
 }
 
 export default App;
