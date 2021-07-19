6822c57186f89bcccf3ad5cf5348d49441894aac
ShiroKatake <tgkhang262@gmail.com>
2021-07-19 20:32:46 +1000

    UPDATE: Changed TodoItem back to functional component
    There were too many instances where I have to use this.props.something, and since using a functional component allows me to shorten it down, and so far I don't have a need for React.Component functions, this is actually better.

--- src/3. TodoList/components/TodoItem.js
+++ src/3. TodoList/components/TodoItem.js
@@ -1,15 +1,13 @@
 import React from "react";
 
-class TodoItem extends React.Component {
-	render() {
-		return (
-			<div className="todo-item">
-				<input type="checkbox" id={this.props.id} name={this.props.name} checked={this.props.checked} />
-				<label for={this.props.name}>{this.props.name}</label>
-				<hr />
-			</div>
-		);
-	}
+function TodoItem(props) {
+	return (
+		<div>
+			<input type="checkbox" id={props.id} name={props.item.name} checked={props.item.checked} onChange={props.handleChange} />
+			<label for={props.item.name}>{props.item.name}</label>
+			<hr />
+		</div>
+	);
 }
 
 export default TodoItem;
