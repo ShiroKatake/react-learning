ed6bac9ad7a377cee06cc0ccd30fa0d7e4aba411
ShiroKatake <tgkhang262@gmail.com>
2021-07-19 13:17:37 +1000

    UPDATE: Changed so app can load data from object file

--- src/3. TodoList/TodoList_App.js
+++ src/3. TodoList/TodoList_App.js
@@ -1,15 +1,13 @@
 import React from "react";
 import TodoComponent from "./components/TodoItem";
+import todo_data from "./todo_data";
 
 class App extends React.Component {
 	render() {
-		return (
-			<div className="todo-list">
-				<TodoComponent />
-				<TodoComponent />
-				<TodoComponent />
-			</div>
-		);
+		const todoComponents = todo_data.map((todo) => {
+			return <TodoComponent id={todo.id} name={todo.name} checked={todo.checked} />;
+		});
+		return <div className="todo-list">{todoComponents}</div>;
 	}
 }
 
