566be777a1f99eba6fc4790f8bdba96aabb99248
ShiroKatake <tgkhang262@gmail.com>
2021-07-19 13:27:52 +1000

    UPDATE: Learned how to import data from object using state

--- src/3. TodoList/TodoList_App.js
+++ src/3. TodoList/TodoList_App.js
@@ -3,9 +3,16 @@ import TodoComponent from "./components/TodoItem";
 import todo_data from "./todo_data";
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			todos: todo_data,
+		};
+	}
+
 	render() {
-		const todoComponents = todo_data.map((todo) => {
-			return <TodoComponent id={todo.id} name={todo.name} checked={todo.checked} />;
+		const todoComponents = this.state.todos.map((item) => {
+			return <TodoComponent id={item.id} name={item.name} checked={item.checked} />;
 		});
 		return <div className="todo-list">{todoComponents}</div>;
 	}
