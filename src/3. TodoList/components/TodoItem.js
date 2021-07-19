082ec66fde882d910de5fc2c6bfc1a929f346d2e
ShiroKatake <tgkhang262@gmail.com>
2021-07-19 13:27:32 +1000

    UPDATE: Fully converted TodoItem to use props from parent class

--- src/3. TodoList/components/TodoItem.js
+++ src/3. TodoList/components/TodoItem.js
@@ -1,22 +1,11 @@
 import React from "react";
 
 class TodoItem extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			id: 0,
-			name: "Do something",
-			isCompleted: false,
-		};
-	}
-
 	render() {
 		return (
 			<div className="todo-item">
-				<input type="checkbox" id={this.state.id} name={this.state.name} checked={this.state.isCompleted} />
-				<label for="clean"> {this.state.name}.</label>
-				<br />
+				<input type="checkbox" id={this.props.id} name={this.props.name} checked={this.props.checked} />
+				<label for={this.props.name}>{this.props.name}</label>
 				<hr />
 			</div>
 		);
