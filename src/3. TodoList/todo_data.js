9e1d28a8dfbcab117bb7e4aafc6ae9bc7cb0f2f5
ShiroKatake <tgkhang262@gmail.com>
2021-07-19 13:15:14 +1000

    ADD: Object data for todo list

---
+++ src/3. TodoList/todo_data.js
@@ -0,0 +1,17 @@
+todo_data = [
+	{
+		id: 1,
+		name: "Wash car.",
+		checked: true,
+	},
+	{
+		id: 2,
+		name: "Learn React",
+		checked: false,
+	},
+	{
+		id: 3,
+		name: "Clean House",
+		checked: true,
+	},
+];
