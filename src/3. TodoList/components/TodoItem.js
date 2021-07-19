ab9d1963f9f273b96711d51a228fd14bb5437230
ShiroKatake <tgkhang262@gmail.com>
2021-07-19 13:17:19 +1000

    UPDATE: Made TodoItem stateful

--- src/3. TodoList/components/TodoItem.js
+++ src/3. TodoList/components/TodoItem.js
@@ -1,12 +1,23 @@
 import React from "react";
 
 class TodoItem extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			id: 0,
+			name: "Do something",
+			isCompleted: false,
+		};
+	}
+
 	render() {
 		return (
 			<div className="todo-item">
-				<input type="checkbox" id="clean" name="isHouseCleaned" />
-				<label for="clean"> Placeholder text.</label>
+				<input type="checkbox" id={this.state.id} name={this.state.name} checked={this.state.isCompleted} />
+				<label for="clean"> {this.state.name}.</label>
 				<br />
+				<hr />
 			</div>
 		);
 	}
