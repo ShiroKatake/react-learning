c67da344a1f6f5f7fa17bdc2e3b6096347eb5a84
ShiroKatake <tgkhang262@gmail.com>
2021-07-19 20:36:13 +1000

    UPDATE: Changed so that the prop itself is passed to the child, not the function

--- src/3. TodoList/components/TodoItem.js
+++ src/3. TodoList/components/TodoItem.js
@@ -3,7 +3,7 @@ import React from "react";
 function TodoItem(props) {
 	return (
 		<div>
-			<input type="checkbox" id={props.id} name={props.item.name} checked={props.item.checked} onChange={props.handleChange} />
+			<input type="checkbox" id={props.id} name={props.item.name} checked={props.item.checked} onChange={props.onChange} />
 			<label for={props.item.name}>{props.item.name}</label>
 			<hr />
 		</div>
