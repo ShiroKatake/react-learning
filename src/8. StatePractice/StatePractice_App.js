3d4cc298936109361227b6666425ac17fd4924a2
ShiroKatake <tgkhang262@gmail.com>
2021-07-19 12:43:43 +1000

    UPDATE: Practicing states

---
+++ src/8. StatePractice/StatePractice_App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            name = "Shiro Katake",
+            age = 21
+        }
+    }
+
+    render() {
+        return(
+          <div>
+              <h1>{this.state.name}</h1>
+              <h3>{this.state.age}</h3>
+          </div>  
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 //import HelloWorld from "./2. HelloWorld/HelloWorld";
 //import ComponentPractice from './3. ComponentPractice/ComponentPractice_App'
-import TodoList from "./3. TodoList/TodoList_App";
+//import TodoList from "./3. TodoList/TodoList_App";
 //import ContactList from './6. ContactList/ContactList_App'
 //import JokeList from "./6. JokeDisplay/JokeList_App";
 //import ProductList from "./7. ProductList/ProductList_App";
+import StatePractice from "./8. StatePractice/StatePractice_App";
 
-ReactDOM.render(<TodoList />, document.getElementById("root"));
+ReactDOM.render(<StatePractice />, document.getElementById("root"));
